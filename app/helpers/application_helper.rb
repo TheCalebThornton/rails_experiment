9dff7b4205c5fed90853ea49d7e3386af67e600c
Thornton <CThornton@pmi.corp>
2017-09-12 14:51:29 -0400

    Adding model with basic tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def in_memory_database?
+    Rails.env.development and Rails.configuration.database_configuration['development']['database'] == ':memory:'
+  end
+
+  if in_memory_database?
+    puts 'creating sqlite in memory database'
+    silence_stream(STDOUT) do
+      load "#{Rails.root}/db/schema.rb"
+    end
+  end
 end
---
+++ db/schema.rb
@@ -0,0 +1,17 @@
+ActiveRecord::Schema.define(version: 20170921173417) do
+
+  create_table 'microposts', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+end
